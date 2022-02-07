2103aee427e0ed1cc8b64df95ca4bbd7146fe035
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2022-02-06 23:32:08 -0500

    more changes

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import crypto from "crypto";
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
